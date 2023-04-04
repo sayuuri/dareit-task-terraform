30726d11f593500aedaf6bbaedcc082e41466809
sayuuri <82342089+sayuuri@users.noreply.github.com>
2023-04-04 17:48:13 +0200

    Feat: Add new bucket from terraform file

--- main.tf
+++ main.tf
@@ -22,27 +22,7 @@ resource "google_compute_instance" "dareit-vm-ci" {
     }
   }
 }
-resource "google_compute_instance" "dareit-vm-ci-2" {
-  name         = "dareit-vm-tf-ci-2"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
-
-  tags = ["dareit"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        managed_by_terraform = "true"
-      }
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
+resource "google_storage_bucket" "terraform_bucket" {
+  name = "terraform_new_bucket"
+  location = "us"
 }
