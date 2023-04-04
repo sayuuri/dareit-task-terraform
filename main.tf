e56513ee16d48c3f29fb1978091da5c657dcfbee
sayuuri <82342089+sayuuri@users.noreply.github.com>
2023-04-04 17:35:53 +0200

    Feat: Add new bucket

--- main.tf
+++ main.tf
@@ -22,3 +22,27 @@ resource "google_compute_instance" "dareit-vm-ci" {
     }
   }
 }
+resource "google_compute_instance" "dareit-vm-ci-2" {
+  name         = "dareit-vm-tf-ci-2"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["dareit"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        managed_by_terraform = "true"
+      }
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
