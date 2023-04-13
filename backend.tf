f838bbce9332c9a61368d8f78b3efa9e296a0efa
sayuuri <82342089+sayuuri@users.noreply.github.com>
2023-04-13 22:51:11 +0200

    Fix: Fix name of bucket

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.0.11"
   backend "gcs" {
-    bucket = "dareit-bucket-tf-sr-2"
+    bucket = "dareit-bucket-tf-sr-1"
     prefix = "dev"
   }
   required_providers {
